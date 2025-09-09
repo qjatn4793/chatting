fbb9b74e12f7da2943e3caae12c8c6d7404a2a32
qjatn4793 <qjatn4793@qjatn4793-IBD1.infobank.net>
2025-09-09 18:11:43 +0900

    [2025-09-09] 주입/저장/브로드캐스트 로직 개선

--- chatting-back/src/main/java/com/realtime/chatting/chat/controller/ChatMessageReceiver.java
+++ chatting-back/src/main/java/com/realtime/chatting/chat/controller/ChatMessageReceiver.java
@@ -2,35 +2,23 @@ package com.realtime.chatting.chat.controller;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.realtime.chatting.chat.entity.RequestChat;
 import com.realtime.chatting.chat.service.ChattingService;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class ChatMessageReceiver {
-
     private final SimpMessagingTemplate messagingTemplate;
-    
-    @Autowired
-    private ChattingService chattingService;
+    private final ChattingService chattingService;
 
-    @Autowired
-    public ChatMessageReceiver(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
-
-    // RabbitMQ로부터 받은 메시지를 WebSocket을 통해 클라이언트로 전송
     public void receiveMessage(RequestChat requestChat) {
-    	
-    	requestChat.setTimestamp(new Date());
-    	
-    	// 메세지 이력 저장
-    	chattingService.saveMessage(requestChat);
-    	
-        // /topic/messages 채널로 메시지를 전송
+        requestChat.setTimestamp(new Date());
+        chattingService.saveMessage(requestChat);
         messagingTemplate.convertAndSend("/topic/messages", requestChat);
     }
 }
