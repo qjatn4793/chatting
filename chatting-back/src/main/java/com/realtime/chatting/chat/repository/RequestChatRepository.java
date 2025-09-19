de440ee5266682013d3f3a3bbf04149c17e3c4c5
qjatn4793 <qjatn4793@host.docker.internal>
2025-09-19 17:12:12 +0900

    [2025-09-19] 불필요 파일들 삭제

--- chatting-back/src/main/java/com/realtime/chatting/chat/repository/RequestChatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.realtime.chatting.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.realtime.chatting.chat.entity.RequestChat;
-
-public interface RequestChatRepository extends JpaRepository<RequestChat, Long> {
-
-}
